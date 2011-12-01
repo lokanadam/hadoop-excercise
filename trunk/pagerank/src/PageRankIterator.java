43335140b7e5dc42b3567383a446399dd7347a13
wujayway@gmail.com <wujayway@gmail.com@062a0adf-df2c-cec4-eb54-fc51605d5a46>
2011-12-01 13:44:33 +0000

    PageRank with static iteration. Fix a bug in GraphBuilder.

--- trunk/pagerank/src/PageRankIterator.java
+++ trunk/pagerank/src/PageRankIterator.java
@@ -2,9 +2,7 @@ import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.hadoop.mapred.*;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.util.*;
 import org.apache.hadoop.io.*;
 
 public class PageRankIterator {
